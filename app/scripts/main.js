c98c2e1fccbe28e9e1a29991aa49fc7582aaae4e
Laura Patti <lpatti10@gmail.com>
2014-08-07 15:14:16 -0400

    Form jquery set-up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,53 @@
-console.log('The Iron Yard Rocks');
+// Ziptastic function working as-is!!
+(function($) {
+    $(function() {
+        var duration = 500;
+
+        var elements = {
+            country: $('#country'),
+            state_short: $('#state-short'),
+            city: $('#city'),
+            zip: $('#zip'),
+            //Additional fields I'm adding to form -LP
+            street: $('#street'),
+            first: $('#first-name'),
+            last: $('#last-name'),
+            phone: $('#phone'),
+            email: $('#email')
+        }
+
+        // Initially hide the city/state/country
+        elements.country.parent().hide();
+        elements.state_short.parent().hide();
+        elements.city.parent().hide();
+
+        //Hiding additional fields added above -LP/////////////////////////////////////////
+        elements.street.parent().hide();
+        elements.first.parent().hide();
+        elements.last.parent().hide();
+        elements.phone.parent().hide();
+        elements.email.parent().hide();
+
+
+        // Initialize the ziptastic and bind to the change of zip code
+        elements.zip.ziptastic()
+            //Calling additional params below -LP/////////////////////////////////////////
+            .on('zipChange', function(evt, country, state_short, city, zip, street, first, last, phone, email) {
+                // Country
+                elements.country.val(country).parent().show(duration);
+
+                // State
+                elements.state_short.val(state_short).parent().show(duration);
+
+                // City
+                elements.city.val(city).parent().show(duration);
+
+                //Showing additional fields added above -LP////////////////////////////////// 
+                elements.street.val(street).parent().show();
+                elements.first.val(first).parent().show();
+                elements.last.val(last).parent().show();
+                elements.phone.val(phone).parent().show();
+                elements.email.val(email).parent().show();
+            });
+    });
+}(jQuery));
